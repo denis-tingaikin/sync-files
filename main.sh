b3222db922b9ea58796efc9bff49d5e3393804b3
Denis Tingajkin <denis.tingajkin@xored.com>
2020-11-28 21:39:46 +0700

    fix issues during testing
    Signed-off-by: Denis Tingajkin <denis.tingajkin@xored.com>

--- main.sh
+++ main.sh
@@ -25,7 +25,7 @@ checkOrAddTemplateIgnore() {
 }
 
 createCommitMessage() {
-    echo "Sync files with ${GITHUB_SERVER_URL}/" >> /tmp/commit-message
+    echo "Sync files with ${GITHUB_SERVER_URL}/${SRC_REPOSITORY}" >> /tmp/commit-message
     echo "" >> /tmp/commit-message
     echo "Version: $(git rev-parse HEAD)"   
     echo "" >> /tmp/commit-message
@@ -33,19 +33,18 @@ createCommitMessage() {
 }
 
 main() {
-    git config --global user.email AUTHOR_EMAIL
-    git config --global user.name AUTHOR_NAME
+    git config --global user.email ${AUTHOR_EMAIL}
+    git config --global user.name ${AUTHOR_NAME}
     currentBranch=$(git branch --show-current)
-    git stash
-    git remote add source ${GITHUB_SERVER_URL}/SRC_REPOSITORY.git
+    git remote add source ${GITHUB_SERVER_URL}/${SRC_REPOSITORY}.git
     git fetch source
-    git checkout source/AUTHOR_EMAIL
+    git checkout source/${SRC_BRANCH_NAME}
     deletedFiles=$(getDeletedFiles)
     createCommitMessage
     git checkout $currentBranch
     checkOrAddTemplateIgnore
-    git diff source/AUTHOR_EMAIL -R | git apply
-    git add $(git ls-tree --name-only -r source/AUTHOR_EMAIL | grep "${REGEX}")
+    git diff source/${SRC_BRANCH_NAME} -R | git apply
+    git add $(git ls-tree --name-only -r source/${SRC_BRANCH_NAME} | grep "${REGEX}")
     git restore -- .templateignore
     while read -r path || [[ -n "$path" ]]; do
         git restore --staged -- $path
@@ -63,10 +62,9 @@ main() {
         exit 0;
     fi
     git commit -s -F /tmp/commit-message
-    git checkout -b sync/SRC_REPOSITORY
-    git push -f origin sync/SRC_REPOSITORY
+    git checkout -b sync/${SRC_REPOSITORY}
+    git push -f origin sync/${SRC_REPOSITORY}
     git checkout $currentBranch
-    git stash pop
 }
 
 main
